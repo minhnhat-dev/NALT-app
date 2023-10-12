e8f31e2dfaa70e58f84982ec687128603d396dfd
David <minhnhat.devweb@gmail.com>
2023-10-12 22:48:25 +0700

    [David] add log

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  console.log("hello")
   return (
     <div className={styles.container}>
       <Head>
