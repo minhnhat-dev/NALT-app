4e41ec86ff5f0a4fac6d4d7511ae3af47f1e6c6a
longngo <longngoryu@gmail.com>
2023-11-08 17:06:52 +0700

    remove multer

--- server/src/app/auth/routes.ts
+++ server/src/app/auth/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import multer from "multer";
 import * as controllers from "./controllers";
 
-const upload = multer();
 const router = Router();
 
-router.post("/signup", upload.none(), controllers.signup);
-router.post("/signin", upload.none(), controllers.signin);
-router.post("/refresh", upload.none(), controllers.refresh);
+router.post("/signup", controllers.signup);
+router.post("/signin", controllers.signin);
+router.post("/refresh", controllers.refresh);
 
 export default router;
