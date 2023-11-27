913b218d6e987b1e263070df6515848b0dcbd45f
longngo <ryu@appurunoMacBook-Air.local>
2023-11-27 20:12:55 +0700

    create environment variables for redis

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -4,4 +4,8 @@ export default {
   accesKey: process.env.ACCESS_KEY!,
   refeshKey: process.env.REFRESH_KEY!,
   port: process.env.PORT || 8000,
+  portRedis: process.env.PORT_REDIS ? parseInt(process.env.PORT_REDIS) : 18774,
+  hostRedis: process.env.HOST_REDIS!,
+  usernameRedis: process.env.USERNAME_REDIS || "default",
+  passwordRedis: process.env.PASSWORD_REDIS!,
 };
--- server/src/database/Redis.ts
+++ server/src/database/Redis.ts
@@ -1,5 +1,5 @@
-import { error, log } from "console";
 import Redis from "ioredis";
+import env from "../config/env";
 
 class Connect {
   private static instance: Connect;
@@ -7,15 +7,15 @@ class Connect {
 
   private constructor() {
     this.redis = new Redis({
-      port: 18774,
-      host: "redis-18774.c1.asia-northeast1-1.gce.cloud.redislabs.com",
-      username: "default",
-      password: "lD8CIY4pkERyeCHG3AVG06nBlflagYsS",
+      port: env.portRedis,
+      host: env.hostRedis,
+      username: env.usernameRedis,
+      password: env.passwordRedis,
     });
 
     this.redis.status === "connecting"
       ? console.log("Connected to Redis")
-      : console.error("Error connecting to Redis:", error);
+      : console.error("Error connecting to Redis!");
   }
 
   public static get Instance() {
